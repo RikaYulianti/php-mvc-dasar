6c46ba824082023029d2118bf2b40dca42e4db76
rikayulianti27 <rikayulianti000@gmail.com>
2021-11-05 10:41:26 +0800

    update phpmvc

---
+++ app/views/user/profile.php
@@ -0,0 +1,4 @@
+<div class="container text-center mt-4">
+ <h1>Halaman User</h1><img height=300 weight=300 src="<?= BASE_URL; ?>/img/rio.jpg" class="rounded-circle shadow">
+ <p>Halo, nama saya <?= $data["nama"]; ?>, saya seorang <?= $data["pekerjaan"];?></p>
+</div>
